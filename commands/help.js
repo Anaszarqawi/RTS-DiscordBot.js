bbf8413eff703241d2a768e046e4f0910f90265a
ANAS ZARQAWI <75158182+Anaszarqawi@users.noreply.github.com>
2022-04-17 22:04:24 +0200

    add about & repo

--- commands/help.js
+++ commands/help.js
@@ -1,24 +1,30 @@
 const Discord = require('discord.js');
-const { MessageEmbed } = require('discord.js');
+const config = require('../config.json');
+
 
 module.exports = {
     name: 'help',
     description: 'use to show all commands',
-    async execute(message, args) {
-        messsage.channel.sendTyping();
-        const helpEmbed = new MessageEmbed()
-            .setColor('#f3f3f3')
+    async execute(message) {
+        const helpEmbed = new Discord.MessageEmbed()
+            .setColor(config.COLOR_EMBED)
             .setTitle('RTS | help')
             .setDescription('Here you can find and check all commands you can trigger for RTS-Bot!')
             .addFields(
                 { name: 'rts', value: 'Send links of RTS Workspace' },
-                { name: '$mute', value: 'Mute all users in the voice channel (admin/MODs only command)' },
-                { name: '$mute @role', value: 'Mute certain roles in the voice channel (admin/MODs only command)' },
-                { name: '$unmute ', value: 'Unmute all users in the voice channel (admin/MODs only command)' },
-                { name: '$unmute @role', value: 'Unmute certain roles in the voice channel (admin/MODs only command)' },
-                { name: '$show', value: 'Returns the amount of users in a voice channel' },
+                { name: '$mute', value: 'Mute all users in the voice channel (MODs Command)' },
+                { name: '$mute @role', value: 'Mute specific role in the voice channel (MODs Command)' },
+                { name: '$mute @user-id', value: 'Mute specific user in the voice channel (MODs Command)' },
+                { name: '$unmute ', value: 'Unmute all users in the voice channel (MODs Command)' },
+                { name: '$unmute @role', value: 'Unmute specific role in the voice channel (MODs Command)' },
+                { name: '$unmute @user-id', value: 'Unmute specific user in the voice channel (MODs Command)' },
+                { name: '$show', value: 'Returns the amount of users in a voice channel (MODs Command)' },
+                { name: '$clear x', value: 'Remove x number of messages (MODs Command)' },
+                { name: '$about', value: 'About RTS WORKSPACE' },
+                { name: '$repo', value: 'Send bot Repository link on github' }
+                
             )
             .setFooter('RTS TEAM')
-        channel.send({ embeds: [helpEmbed] });
+        message.channel.send({ embeds: [helpEmbed] });
     }
 };
